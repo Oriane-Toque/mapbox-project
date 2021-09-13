18b9917a402f2ab176bed27263645e3293af563c
Oriane-Toque <oriane.toque@gmail.com>
2021-09-13 14:14:12 +0200

    win - data fixtures + timestamps trait

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Event;
+use Faker;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,8 +11,25 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $event = new Event();
 
-        $manager->flush();
+        $faker = Faker\Factory::create('fr_FR');
+
+        for ($i = 0; $i < 30; $i++) {
+            $event = new Event();
+
+            $event->setTitle('Event ' . $i);
+            $event->setDescription($faker->text(300));
+            $event->setDate($faker->dateTimeBetween('+1 week', '+3 week'));
+            $event->setAddress($faker->address());
+            $event->setLat($faker->latitude());
+            $event->setLon($faker->longitude());
+            $event->setMaxAttendants(rand(1, 20));
+            $event->setnbAttendants(rand(1, 20));
+            $event->setAuthor($faker->name());
+
+
+            $manager->persist($event);
+            $manager->flush();
+        }
     }
 }
